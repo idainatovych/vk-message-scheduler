09c029fb20dd7416d1a0c4331130064331429a76
Ilya Daynatovich <shupuercha@gmail.com>
2016-11-13 12:20:13 +0200

    Add redux

--- app/main.js
+++ app/main.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/app/app';
 
 injectTapEventPlugin();
 
-const anchor = document.getElementById('app');
-ReactDOM.render(<App/>, anchor);
+const reducer = (state = {}, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+const render = () => {
+  const anchor = document.getElementById('app');
+  ReactDOM.render(<App/>, anchor);
+};
+
+store.subscribe(render);
+render();
