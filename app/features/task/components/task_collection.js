d2d7ba2c39dc3c38ffb17c504f957e1ebe7fa27f
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2016-11-25 01:10:26 +0200

    Updated tasks rendering with real props

--- app/features/task/components/task_collection.js
+++ app/features/task/components/task_collection.js
@@ -1,32 +1,31 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-import {GridList, GridTile} from 'material-ui/GridList';
+import { GridList, GridTile } from 'material-ui/GridList';
 import Subheader from 'material-ui/Subheader';
 import IconButton from 'material-ui/IconButton';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 
-let tasks = new Array(6).fill('');
-
 const iconButton = <IconButton><ActionInfo color="white"/></IconButton>
 
-let mocks;
-
-if (tasks.length > 0) {
-  mocks = tasks.map((el, i) => (
-    <GridTile title="Lorem"
-              key={i}
-              actionIcon={iconButton}
-              subtitle="John Doe at 12.45 pm">
-      <img src="http://lorempixel.com/300/300/cats"/>
-    </GridTile>
-  ));
-} else {
-  mocks = <p>Empty, click action button to create.</p>
-}
-
 class TaskCollection extends React.Component {
+  renderSchedule() {
+    if (this.props.tasks.length > 0) {
+      return this.props.tasks.map((event, i) => (
+        <GridTile title={event.title}
+                  key={i}
+                  actionIcon={iconButton}
+                  subtitle='John Doe at 12.45 pm'>
+          <img src="http://lorempixel.com/300/300/cats"/>
+        </GridTile>
+      ));
+    } else {
+      return <p>Empty, click action button to create.</p>
+    }
+  }
+
   render() {
+    const mocks = this.renderSchedule();
     return (
       <div className="task-collection">
         <GridList>
@@ -37,7 +36,9 @@ class TaskCollection extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  tasks: state.tasks
+});
 
 const mapDispatchToProps = (dispatch) => ({});
 
