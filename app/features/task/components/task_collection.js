879acc0feddd130d530c9c4b36a309d978131c77
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2016-11-24 23:49:53 +0200

    Emulated component with for empty schedule

--- app/features/task/components/task_collection.js
+++ app/features/task/components/task_collection.js
@@ -1,66 +1,44 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-import { GridList, GridTile } from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList';
 import Subheader from 'material-ui/Subheader';
 import IconButton from 'material-ui/IconButton';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 
-
-let tasks = [];
+let tasks = new Array(6).fill('');
 
 const iconButton = <IconButton><ActionInfo color="white"/></IconButton>
+
+let mocks;
+
+if (tasks.length > 0) {
+  mocks = tasks.map((el, i) => (
+    <GridTile title="Lorem"
+              key={i}
+              actionIcon={iconButton}
+              subtitle="John Doe at 12.45 pm">
+      <img src="http://lorempixel.com/300/300/cats"/>
+    </GridTile>
+  ));
+} else {
+  mocks = <p>Empty, click action button to create.</p>
+}
 
 class TaskCollection extends React.Component {
   render() {
     return (
       <div className="task-collection">
         <GridList>
-          <Subheader>Saturday</Subheader>
-          <GridTile key={1} title="Lorem"
-                    actionIcon={iconButton}
-                    subtitle="John Doe at 12.45 pm">
-            <img src="http://lorempixel.com/300/300/cats"/>
-          </GridTile>
-          <GridTile key={2} title="Ipsum"
-                    actionIcon={iconButton}
-                    subtitle="John Doe at 12.45 pm">
-            <img src="http://lorempixel.com/300/300/cats"/>
-          </GridTile>
-          <Subheader>Sunday</Subheader>
-          <GridTile key={3} title="Dolor"
-                    actionIcon={iconButton}
-                    subtitle="John Doe at 12.45 pm">
-            <img src="http://lorempixel.com/300/300/cats"/>
-          </GridTile>
-          <GridTile key={4} title="Sit amet"
-                    actionIcon={iconButton}
-                    subtitle="John Doe at 12.45 pm">
-            <img src="http://lorempixel.com/300/300/cats"/>
-          </GridTile>
-          <Subheader>Monday</Subheader>
-          <GridTile key={5} title="Dolor"
-                    actionIcon={iconButton}
-                    subtitle="John Doe at 12.45 pm">
-            <img src="http://lorempixel.com/300/300/cats"/>
-          </GridTile>
-          <GridTile key={6} title="Sit amet"
-                    actionIcon={iconButton}
-                    subtitle="John Doe at 12.45 pm">
-            <img src="http://lorempixel.com/300/300/cats"/>
-          </GridTile>
+          {mocks}
         </GridList>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({});
 
-});
-
-const mapDispatchToProps = (dispatch) => ({
-
-});
+const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(TaskCollection);
