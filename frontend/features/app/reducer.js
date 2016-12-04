840eaa2c5420bb3fe78a72748f226646ac2c83a5
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2016-12-04 20:50:02 +0200

    Removed browserHistory in favor of Links

--- frontend/features/app/reducer.js
+++ frontend/features/app/reducer.js
@@ -1,5 +1,4 @@
 import AppActionKeys from './keys';
-import { browserHistory } from 'react-router'
 
 const defaultState = {
   isMenuOpen: false,
@@ -18,22 +17,18 @@ export default (state = defaultState, action) => {
         isMenuOpen: false
       });
     case AppActionKeys.OPEN_CREATE_TASK_DIALOG:
-      browserHistory.push('/create-task');
       return Object.assign({}, state, {
         isCreateTaskDialogOpen: true
       });
     case AppActionKeys.CLOSE_CREATE_TASK_DIALOG:
-      browserHistory.goBack();
       return Object.assign({}, state, {
         isCreateTaskDialogOpen: false
       });
     case AppActionKeys.OPEN_EDIT_TASK_DIALOG:
-      browserHistory.push('/edit-task');
       return Object.assign({}, state, {
         isEditTaskDialogOpen: true
       });
     case AppActionKeys.CLOSE_EDIT_TASK_DIALOG:
-      browserHistory.goBack();
       return Object.assign({}, state, {
         isEditTaskDialogOpen: false
       });
